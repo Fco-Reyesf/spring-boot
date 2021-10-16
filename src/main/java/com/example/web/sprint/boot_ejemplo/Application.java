4a5ec768370d9eac9f6c68e87c1bd646d3bba0a1
Fco-Reyesf <freyes.info@gmail.com>
2021-10-15 23:36:32 -0300

    hello world

--- src/main/java/com/example/web/sprint/boot_ejemplo/Application.java
+++ src/main/java/com/example/web/sprint/boot_ejemplo/Application.java
@@ -2,12 +2,24 @@ package com.example.web.sprint.boot_ejemplo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name)
+	{
+		return String.format("Hello %s!", name);
 	}
 
 }
